ebb499fbe44cd3bf5bde568d173d921e4d38888a
Controforme <ciao@controforme.com>
2025-09-06 17:31:32 +0200

    added TileMap.js basic methods for map drawing

---
+++ src/Game.js
@@ -0,0 +1,21 @@
+/*import*/
+import TileMap from "./TileMap.js";
+
+/*variables*/
+const canvas = document.getElementById("gameCanvas"); //select the html canvas
+const ctx = canvas.getContext("2d"); //two dimensional rendering context
+const glInterval = 1000 / 75; //interval between consecutive gameLoop redraws
+const tileX = 48; //width of a single tile
+const tileY = 16; //height of a single tile
+const tileMap = new TileMap(tileX, tileY); //build a tile map
+
+/*game loop - redraws the screen*/
+function gameLoop() {
+  tileMap.draw(ctx);
+}
+
+/*set canvas size*/
+tileMap.setCanvasSize(canvas);
+
+/*call game loop every x milliseconds set in glInterval*/
+setInterval(gameLoop, glInterval);
---
+++ src/TileMap.js
@@ -0,0 +1,71 @@
+/*methods to build the tile map*/
+export default class TileMap {
+  constructor(tileX, tileY) {
+    this.tileX = tileX;
+    this.tileY = tileY;
+
+    this.emptyCell = new Image();
+    this.emptyCell.src = "../images/emptyCell.png";
+
+    this.wall = new Image();
+    this.wall.src = "../images/wall.png";
+  }
+
+  // array that represents the map
+  map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+
+  draw(ctx) {
+    //draw the map based on the map array
+    for (let row = 0; row < this.map.length; row++) {
+      //let is a variable that has block scope
+      for (let column = 0; column < this.map[row].length; column++) {
+        let tile = this.map[row][column];
+        if (tile === 1) {
+          this.#drawWall(ctx, column, row, this.tileX, this.tileY);
+        } else if (tile === 0) {
+          this.#drawEmpty(ctx, column, row, this.tileX, this.tileY);
+        }
+      }
+    }
+  }
+
+  //draw the walls
+  //methods that start with # are private, can't be called outside this file
+  #drawWall(ctx, column, row, sizeX, sizeY) {
+    ctx.drawImage(
+      this.wall,
+      column * this.tileX,
+      row * this.tileY,
+      sizeX,
+      sizeY
+    );
+  }
+
+  //draw the empty cells
+  #drawEmpty(ctx, column, row, sizeX, sizeY) {
+    ctx.drawImage(
+      this.emptyCell,
+      column * this.tileX,
+      row * this.tileY,
+      sizeX,
+      sizeY
+    );
+  }
+
+  //sets width and height of the canvas based on the map array and the tile size
+  setCanvasSize(canvas) {
+    canvas.width = this.map[0].length * this.tileX;
+    canvas.height = this.map.length * this.tileY;
+  }
+}
